677e9bf4878b80d2c8b9d00565df15840ffebcfe
Alan Villarreal <alanmvh@gmail.com>
2022-06-29 22:53:47 -0500

    add: Functions folder

---
+++ Functions/greeter.py
@@ -0,0 +1,13 @@
+def greet_user(username):
+    """Display s simple greeting."""
+    print("Hello " + username.title() + "!")
+
+def display_message():
+    print("I am leearning python with the python crash course book")
+
+def favorite_book(title):
+    print("One of my favorite books is: " + title.title()) 
+
+display_message()
+favorite_book("Python crash course")
+greet_user("Jeese")
